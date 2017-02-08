c01ea92400fb45e8d359c402ae7cd3e14f94dbcc
NatashaMitchko1 <natasha.mitchko@gmail.com>
2017-02-08 18:57:49 -0500

    Works with examples gives
    Removed the arrays and worked only with the input string and values at each index. While loop runs while the current index being looked at is less than the length of the string input. If the current character at index i is a number look at value at (the current index + the number + 1) and append it to the string "decoded". If the current character is not a number move to the next index (I don't know if this is necessary if everything is coded the same way). Does not have inline comments

--- tester_workspace.py
+++ tester_workspace.py
@@ -1,26 +1,16 @@
-def func(str):
+def plaintext(str):
     i = 0
     y = 0
-    splitString = []
     intSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    numberInfo = []
     decoded = ""
     stringLen = len(str)
+    print (stringLen)
     while i < stringLen:
         current_char = str[i]
-        splitString.append(current_char)
         if any(x in current_char for x in intSet):
-            numberInfo.append(int(current_char))
-        i = i + 1
-    print (splitString)
-    print (numberInfo)
-    #below is not correct, not getting num of char correct
-    #not getting character itself correct (takes first char and doesn't update)
-    for x in splitString:
-        if any(character in x for character in intSet):
-            decodedValue = (splitString[y+1])
-            nextIndex = int((splitString[y]))
-            decoded = decoded + decodedValue
+            i = int(current_char) + i + 1
+            decoded = decoded + str[i]
+            i = i + 1
         else:
-            y = y + nextIndex
+            i = i + 1
     print (decoded)
